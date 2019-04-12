8e09ef0e9b021d44b6351f7c97940b46310904f0
nghiemdao <nghiemvandao@gmail.com>
2019-04-12 15:44:09 +0700

    Remove clean webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlBeautifyPlugin = require('html-beautify-webpack-plugin');
@@ -26,7 +25,6 @@ module.exports = {
     }
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new webpack.LoaderOptionsPlugin({
       options: {
         handlebarsLoader: {}
